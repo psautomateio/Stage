59d2e3aaacb5920696bfec6275b06283763e5d75
psautomateio <56523654+psautomateio@users.noreply.github.com>
2019-10-21 19:52:11 +0100

    Update apps.sh
    bumped version

--- psacloner/apps.sh
+++ psacloner/apps.sh
@@ -12,7 +12,7 @@
 echo 'apps' > /psa/var/psacloner.rolename
 echo 'Apps' > /psa/var/psacloner.roleproper
 echo 'Apps' > /psa/var/psacloner.projectname
-echo 'master' > /psa/var/psacloner.projectversion
+echo '0.2.0' > /psa/var/psacloner.projectversion
 echo 'psabox.sh' > /psa/var/psacloner.startlink
 
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
